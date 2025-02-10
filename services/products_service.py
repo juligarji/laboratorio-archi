87e0c9d708f1ffe0bdd4927f22dfa0f1f48a4253
Julian Garzon <julian.garzon@sonder.com>
2025-02-09 23:23:33 -0500

    initial commit

---
+++ services/products_service.py
@@ -0,0 +1,6 @@
+from repositories.products_repository import ProductRepository
+
+class ProductService:
+  @staticmethod
+  def create_product(name, description):
+    return ProductRepository.create_product(name, description)
